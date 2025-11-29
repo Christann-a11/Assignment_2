74ac0d01af4a4c19c3d3543a5041ad82a1325d11
Christann Groves <cgroves6@my.centennialcollege.ca>
2025-11-29 16:13:48 -0500

    Refactor project routes to include authentication

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var projectController = require('../controllers/projects');
 
-<<<<<<< HEAD
+
 const auth = require('../middleware/auth');
 
 
@@ -12,13 +12,6 @@ router.get('/:projectId', projectController.getProject);
 router.put('/:projectId', auth, projectController.update);
 router.delete('/:projectId', auth, projectController.remove);
 router.delete('/', auth, projectController.removeAll);
-=======
-router.get('/', projectController.getAll);
-router.post('/', projectController.create);
-router.get('/:projectId', projectController.getProject);
-router.put('/:projectId', projectController.update);
-router.delete('/:projectId', projectController.remove);
-router.delete('/', projectController.removeAll);
->>>>>>> 9ca08161849e86f9901efd5e4cb5bf18ac0035d1
+
 
 module.exports = router;
