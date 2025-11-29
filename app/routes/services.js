bd09aed2e47a305f2aaab76c742e84597e4b08ee
Christann Groves <cgroves6@my.centennialcollege.ca>
2025-11-29 16:14:10 -0500

    Refactor services routes to eliminate duplicates
    Removed duplicate route definitions for services.

--- app/routes/services.js
+++ app/routes/services.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var serviceController = require('../controllers/services');
 
-<<<<<<< HEAD
 
 const auth = require('../middleware/auth');
 
@@ -12,13 +11,5 @@ router.get('/:serviceId', serviceController.getService);
 router.put('/:serviceId', auth, serviceController.update);
 router.delete('/:serviceId', auth, serviceController.remove);
 router.delete('/', auth, serviceController.removeAll);
-=======
-router.get('/', serviceController.getAll);
-router.post('/', serviceController.create);
-router.get('/:serviceId', serviceController.getService);
-router.put('/:serviceId', serviceController.update);
-router.delete('/:serviceId', serviceController.remove);
-router.delete('/', serviceController.removeAll);
->>>>>>> 9ca08161849e86f9901efd5e4cb5bf18ac0035d1
 
 module.exports = router;
